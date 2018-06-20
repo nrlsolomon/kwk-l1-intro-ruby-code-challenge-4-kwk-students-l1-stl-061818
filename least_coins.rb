cb6089c6cfa02ce7ec2d359b8dc781310a244e1b
nrlsolomon <nrlsolom5@gmail.com>
2018-06-20 19:45:56 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,9 @@
 #write out your code here
 
 def least_coins(cents)
+coins = {"quarters" => 0.25, "dimes" => 0.10, "nickels" => 0.5, "pennies" => 0.1}
 
-#Code your answer here!
 
 end
+
+least_coins
